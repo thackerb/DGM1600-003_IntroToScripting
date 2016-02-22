3e5b8eeded97b43f4925b57bb58964bb6902d86b
Bryan Thacker <brythacker@gmail.com>
2016-02-22 09:45:17 -0700

    GitHub Flow assignment.

---
+++ Assignments/Assets/GitHubFlow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitHubFlow : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+        int duck = 2;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
