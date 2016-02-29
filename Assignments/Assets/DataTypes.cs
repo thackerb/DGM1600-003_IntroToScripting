41acb644193e6ae33827e1436598c3b0540ed3ec
Bryan Thacker <brythacker@gmail.com>
2016-02-28 23:05:01 -0700

    Data Types Assignment

---
+++ Assignments/Assets/DataTypes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataTypes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
+
+        //Transform tran = transform;
+        //tran.position = new Vector3(0, 2, 0);         
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
