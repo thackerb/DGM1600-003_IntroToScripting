2195d6b73a59f3e3a14d0333892a8cf77c150d03
Bryan Thacker <brythacker@gmail.com>
2016-03-11 09:57:14 -0700

    Enum assignment update

---
+++ Assignments/Assets/Enum3.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enum3 : MonoBehaviour
+{
+    public enum directions
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    void ChangeDirection(directions _d)
+    {
+        currentDirection = _d;
+        print(currentDirection);
+    }
+
+    public directions currentDirection;
+
+    void OnMouseDown()
+    {
+
+        switch (currentDirection)
+        {
+            case directions.North:
+                ChangeDirection(directions.South);
+                break;
+
+            case directions.South:
+                ChangeDirection(directions.East);
+                break;
+
+            case directions.East:
+                ChangeDirection(directions.West);
+                break;
+
+            case directions.West:
+                ChangeDirection(directions.North);
+                break;
+        }
+    } 
+
+    // Update is called once per frame
+    void Update () 
+    {
+	
+	}
+}
